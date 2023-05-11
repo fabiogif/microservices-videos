93e51e58300d394249e3afe6767b6290d1b71cda
fabiogif <fabiosantanagif@gmail.com>
2023-05-11 20:19:22 -0300

    Adicionando id na entidade categoria

---
+++ src/category/domain/entities/category.spec.ts
@@ -0,0 +1,133 @@
+import { Category, CategoryProperties } from "./category";
+import { omit } from 'lodash';
+import { validate as uuidValidate} from 'uuid';
+
+describe('Category Tests', () => {
+    test('constructor of category', () => {
+
+       let category = new Category({
+            name: 'Movie'
+        }, null);
+        let props = omit( category.props, 'created_at');
+        expect( category.props.created_at).toBeInstanceOf(Date);
+
+        category = new Category({
+            name: "Movie",
+            description: "some description",
+            is_active: false
+        }, null)
+        let created_at = new Date();
+        expect(category.props).toStrictEqual({
+            name: 'Movie',
+            description: "some description",
+            is_active: false,
+            created_at,
+        });
+
+
+        category = new Category({
+            name: "Movie",
+            description: "other description",
+        }, undefined)
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            description: "other description"
+        });
+
+        category = new Category({
+            name: "Movie",
+            is_active: true
+        })
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            is_active: true
+        });
+
+        created_at = new Date();
+        category = new Category({
+            name: "Movie",
+            created_at
+        })
+        expect(category.props).toMatchObject({
+            name: 'Movie',
+            created_at
+        });
+    })
+
+    test('getter of name field', () => {
+        const category = new Category({ name: 'Movie'});
+        expect(category.name).toBe("Movie");
+    })
+
+
+    test('getter of description field', () => {
+        let category = new Category({
+            name: "Movie"});
+            expect(category.description).toBeNull();
+
+        category = new Category({
+                name: "Movie",
+            description: 'some description'});
+        expect(category.description).toBe("some description");
+
+        category = new Category({
+            name: "Movie",
+        });
+        category["description"] = "other description";
+        expect(category.description).toBe("other description");
+
+        category["description"] = undefined;
+        expect(category.description).toBeNull();
+    })
+
+    test("getter and setter of is_active prop", ()=>{
+        let category = new Category({
+            name: "Movie",
+        });
+        expect(category.is_active).toBeTruthy();
+
+        category = new Category({
+            name: "Movie",
+            is_active: true
+        });
+        expect(category.is_active).toBeTruthy();
+        category = new Category({
+            name: "Movie",
+            is_active: false
+        });
+        expect(category.is_active).toBeFalsy();
+    });
+
+    test("getter of created_at prop", () => {
+        let category = new Category({
+            name: "Movie"
+        })
+        expect(category.created_at).toBeInstanceOf(Date);
+       
+        let created_at = new Date();
+        category = new Category({
+            name: "Movie",
+            created_at
+        }); 
+        expect(category.created_at).toBe(created_at);
+    })
+
+    test("id field", () =>{
+        type CategoryData = { props: CategoryProperties; id?: string};
+        const data:CategoryData[] = [
+            {props: {name: 'Movie'}},
+            {props: {name: 'Movie'}, id: null},
+            {props: {name: 'Movie'}, id: undefined},
+            {props: {name: 'Movie'}, id: '86184aa9-5156-460f-9e11-8b6e5fb7b000'},
+        ];
+
+        data.forEach((i) => {
+            const category = new Category(i.props, i.id);
+            expect(category.id).not.toBeNull();
+            expect(uuidValidate(category.id)).toBeTruthy();
+        });
+
+    })
+
+})
+
